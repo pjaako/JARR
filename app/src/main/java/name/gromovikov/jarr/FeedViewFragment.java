9906f3f4d12b40fbdf8afedcd529da813ab45b07
pjaako <pavel.gromovikov@gmail.com>
2015-11-09 12:00:00 +0300

    Initial commit

---
+++ app/src/main/java/name/gromovikov/jarr/FeedViewFragment.java
@@ -0,0 +1,22 @@
+package name.gromovikov.jarr;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FeedViewFragment extends Fragment {
+
+    public FeedViewFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feed_view, container, false);
+    }
+}
