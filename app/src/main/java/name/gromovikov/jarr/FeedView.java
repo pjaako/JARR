aedcc86872388b47be22c3f901bb1a759ab1d794
pja <pavel.gromovikov@gmail.com>
2015-11-11 23:38:55 +0300

    two-pane layout on tablet in landscape (and a bunch of layout bugs) added

--- app/src/main/java/name/gromovikov/jarr/FeedView.java
+++ app/src/main/java/name/gromovikov/jarr/FeedView.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,10 +23,11 @@ public class FeedView extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_view);
+        /* we probably will return to this later - pja
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /* we probably will return to this later - pja
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,7 +43,6 @@ public class FeedView extends AppCompatActivity
             feedViewFragment.setArguments(getIntent().getExtras());
             FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
             fragmentTransaction.add(R.id.fragment, feedViewFragment);
-
             fragmentTransaction.commit();
         }
 
@@ -59,9 +60,15 @@ public class FeedView extends AppCompatActivity
         Bundle bundle = new Bundle();
         bundle.putString("url", url);
         postViewFragment.setArguments(bundle);
-        fragmentTransaction.replace(R.id.fragment, postViewFragment);
-
-        fragmentTransaction.addToBackStack(null);
+        //set the post in special fragment if available
+        if (findViewById(R.id.post_fragment) != null){
+            fragmentTransaction.replace(R.id.post_fragment, postViewFragment);
+        }
+        //or insteadof the feed if no special place is found (stash feed to put back on "back" press)
+        else {
+            fragmentTransaction.replace(R.id.fragment, postViewFragment);
+            fragmentTransaction.addToBackStack(null);
+        }
         // Commit the transaction
         fragmentTransaction.commit();
 
