e247b5d7c8fadfd8d686325793b57c639603b5c0
pjaako <pavel.gromovikov@gmail.com>
2015-11-12 19:14:50 +0300

    adding DB for feeds - broken commit

---
+++ app/src/main/java/name/gromovikov/jarr/FeedDb.java
@@ -0,0 +1,74 @@
+package name.gromovikov.jarr;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
+
+/**
+ * Created by pja on 12.11.2015.
+ * based on http://developer.android.com/intl/ru/training/basics/data-storage/databases.html
+ */
+public class FeedDb  {
+
+    public final class FeedReaderContract {
+        // To prevent someone from accidentally instantiating the contract class,
+        // give it an empty constructor.
+        public FeedReaderContract() {}
+
+        /* Inner class that defines the table contents */
+        public abstract class FeedEntry implements BaseColumns {
+            public static final String TABLE_NAME = "entry";
+            public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+            public static final String COLUMN_NAME_TITLE = "title";
+            public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+
+        }
+    }
+
+    public class FeedDbHelper extends SQLiteOpenHelper {
+        // If you change the database schema, you must increment the database version.
+        public static final int DATABASE_VERSION = 1;
+        public static final String DATABASE_NAME = "FeedReader.db";
+
+        public FeedDbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_ENTRIES);
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // This database is only a cache for online data, so its upgrade policy is
+            // to simply to discard the data and start over
+            db.execSQL(SQL_DELETE_ENTRIES);
+            onCreate(db);
+        }
+        public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            onUpgrade(db, oldVersion, newVersion);
+        }
+    }
+
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "FeedReader.db";
+
+    public FeedDb(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
