54a68904ae97cf7a2078041f2e853102f6684c9d
pja <pavel.gromovikov@gmail.com>
2015-11-12 22:42:24 +0300

    database class un-broken

--- app/src/main/java/name/gromovikov/jarr/FeedDb.java
+++ app/src/main/java/name/gromovikov/jarr/FeedDb.java
@@ -5,33 +5,50 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
+import java.util.List;
+
 /**
  * Created by pja on 12.11.2015.
  * based on http://developer.android.com/intl/ru/training/basics/data-storage/databases.html
  */
 public class FeedDb  {
 
-    public final class FeedReaderContract {
-        // To prevent someone from accidentally instantiating the contract class,
-        // give it an empty constructor.
-        public FeedReaderContract() {}
 
-        /* Inner class that defines the table contents */
-        public abstract class FeedEntry implements BaseColumns {
-            public static final String TABLE_NAME = "entry";
-            public static final String COLUMN_NAME_ENTRY_ID = "entryid";
-            public static final String COLUMN_NAME_TITLE = "title";
-            public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    /* Inner class that defines the table contents */
+    public abstract class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
 
-        }
     }
 
-    public class FeedDbHelper extends SQLiteOpenHelper {
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedEntry.COLUMN_NAME_ENTRY_ID + TEXT_TYPE + COMMA_SEP +
+                    FeedEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+
+            " )";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+
+    public void addFromFeed(List<RssRecord> feed){
+        for (RssRecord postMeta : feed){
+
+        }
+
+    }
+
+    public class Helper extends SQLiteOpenHelper {
         // If you change the database schema, you must increment the database version.
         public static final int DATABASE_VERSION = 1;
         public static final String DATABASE_NAME = "FeedReader.db";
 
-        public FeedDbHelper(Context context) {
+        public Helper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
 
@@ -50,25 +67,4 @@ public class FeedDb  {
         }
     }
 
-    // If you change the database schema, you must increment the database version.
-    public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "FeedReader.db";
-
-    public FeedDb(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
-
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SQL_CREATE_ENTRIES);
-    }
-
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // This database is only a cache for online data, so its upgrade policy is
-        // to simply to discard the data and start over
-        db.execSQL(SQL_DELETE_ENTRIES);
-        onCreate(db);
-    }
-    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        onUpgrade(db, oldVersion, newVersion);
-    }
 }
